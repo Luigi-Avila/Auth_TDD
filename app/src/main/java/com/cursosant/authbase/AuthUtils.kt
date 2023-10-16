8eb248d68321f6ea1eb6f6e69a99d33642558b91
Luigi <l.avila.uvg@gmail.com>
2023-10-16 17:16:16 -0400

    feat: code refactored

--- app/src/main/java/com/cursosant/authbase/AuthUtils.kt
+++ app/src/main/java/com/cursosant/authbase/AuthUtils.kt
@@ -8,21 +8,22 @@ fun userAuthentication(email: String, password: String): Boolean {
 }
 
 fun userAuthenticationTDD(email: String?, password: String?): AuthEvents {
-    if (email == null && password == null) throw AuthException(AuthEvents.NULL_FORM)
-    if (email == null) throw AuthException(AuthEvents.NULL_EMAIL)
-    if (password == null) throw AuthException(AuthEvents.NULL_PASSWORD)
-
-    return if (email.isBlank() && password.isBlank()) AuthEvents.FORM_EMPTY
-    else if (email.isBlank()) AuthEvents.EMAIL_EMPTY
-    else if(password.isBlank()) AuthEvents.PASSWORD_EMPTY
-    else if (password.length < 4) AuthEvents.SHORT_PASSWORD
-    else {
-        val passwordNumber = password.toIntOrNull()
-        return if (!isEmailValid(email) && passwordNumber == null) AuthEvents.INVALID_USER
-        else if (!isEmailValid(email)) AuthEvents.INVALID_EMAIL
-        else if (passwordNumber == null) AuthEvents.INVALID_PASSWORD
-        else {
-            return if (email == "luigi@gmail.com" && password == "1234") AuthEvents.USER_EXIST else AuthEvents.NOT_USER_EXIST
+    return when {
+        email == null && password == null -> throw AuthException(AuthEvents.NULL_FORM)
+        email == null -> throw AuthException(AuthEvents.NULL_EMAIL)
+        password == null -> throw AuthException(AuthEvents.NULL_PASSWORD)
+        email.isBlank() && password.isBlank() -> AuthEvents.FORM_EMPTY
+        email.isBlank() -> AuthEvents.EMAIL_EMPTY
+        password.isBlank() -> AuthEvents.PASSWORD_EMPTY
+        password.length < 4 -> AuthEvents.SHORT_PASSWORD
+        else -> {
+            val passwordNumber = password.toIntOrNull()
+            when {
+                !isEmailValid(email) && passwordNumber == null -> AuthEvents.INVALID_USER
+                !isEmailValid(email) -> AuthEvents.INVALID_EMAIL
+                passwordNumber == null -> AuthEvents.INVALID_PASSWORD
+                else -> if (email == "luigi@gmail.com" && password == "1234") AuthEvents.USER_EXIST else AuthEvents.NOT_USER_EXIST
+            }
         }
     }
 }
